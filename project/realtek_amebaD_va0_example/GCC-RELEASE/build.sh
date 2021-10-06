6e025a01389943fbb21c6fc0342d1c1e62c4f6a5
pankore <86098180+pankore@users.noreply.github.com>
2021-10-06 11:48:30 +0800

    [CMAKE] Rename to MATTER_PATH

--- project/realtek_amebaD_va0_example/GCC-RELEASE/build.sh
+++ project/realtek_amebaD_va0_example/GCC-RELEASE/build.sh
@@ -18,17 +18,18 @@ else
    echo "Toolchain $(ls -A $CMAKE_ROOT/toolchain/linux) is found at $CMAKE_ROOT/toolchain/linux."
 fi
 
-##CHIP_PATH to be defined in MATTER SDK
-if [ ! -z ${CHIP_PATH} ]; then
-    echo "CHIP_PATH is located at: ${CHIP_PATH}" 
+##MATTER_PATH to be defined in MATTER SDK
+if [ ! -z ${MATTER_PATH} ]; then
+    echo "MATTER_PATH is located at: ${MATTER_PATH}" 
 else
-    echo "Error: CHIP_PATH does not defined."
+    echo "Error: MATTER_PATH does not defined."
     exit
 fi
-export CONFIG_PATH=${CHIP_PATH}/config/ameba
-export EXAMPLE_PATH=${CHIP_PATH}/examples/all-clusters-app/ameba
 
-cd $CHIP_PATH
+export CHIP_CONFIG_PATH=${MATTER_PATH}/config/ameba
+export CHIP_EXAMPLE_PATH=${MATTER_PATH}/examples/all-clusters-app/ameba
+
+cd $MATTER_PATH
 if [ ! -d "out" ]; then
     mkdir out
 fi
@@ -49,15 +50,15 @@ else
 	#make
 fi
 
-if [ -a "$LP_IMAGE/km0_boot_all.bin" ]; then
-    cp $LP_IMAGE/km0_boot_all.bin $CHIP_PATH/out/asdk/image/km0_boot_all.bin
-else
-    echo "Error: km0_boot_all.bin can not be found." 
-fi
+#if [ -a "$LP_IMAGE/km0_boot_all.bin" ]; then
+#    cp $LP_IMAGE/km0_boot_all.bin $MATTER_PATH/out/asdk/image/km0_boot_all.bin
+#else
+#    echo "Error: km0_boot_all.bin can not be found."
+#fi
 
-if [ -a "$HP_IMAGE/km4_boot_all.bin" ]; then
-    cp $HP_IMAGE/km4_boot_all.bin $CHIP_PATH/out/asdk/image/km4_boot_all.bin
-else
-    echo "Error: km4_boot_all.bin can not be found."
-fi
+#if [ -a "$HP_IMAGE/km4_boot_all.bin" ]; then
+#    cp $HP_IMAGE/km4_boot_all.bin $MATTER_PATH/out/asdk/image/km4_boot_all.bin
+#else
+#    echo "Error: km4_boot_all.bin can not be found."
+#fi
 
